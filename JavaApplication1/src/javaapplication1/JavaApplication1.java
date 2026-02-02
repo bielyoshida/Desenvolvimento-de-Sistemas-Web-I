0fe95aceb694c60f615af7d61f73ec17ae8b830b
bielyoshida <bielyoshida@gmail.com>
2026-02-02 20:57:04 -0300

    Add files via upload

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        pessoa individuo = new pessoa();
+        System.out.println("Informe seu nome");    
+        individuo.nome = ler.next();
+        individuo.apresentação();
+        
+        
+    }
+    
+}
---
+++ JavaApplication1/src/javaapplication1/pessoa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author User
+ */
+public class pessoa {
+    String nome;
+    int idade;
+    float peso, altura;
+ 
+    public void apresentação(){
+        System.out.println("oi, sou "+nome);
+    }
+  
+}
